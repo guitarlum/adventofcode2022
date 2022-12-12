6ecc0e3cb85c01911473ff928d43427dbe1ae2fd
Steffen Dangmann <steffen.dangmann@regnology.net>
2022-12-12 22:18:17 +0100

    part 01

--- day12.go
+++ day12.go
@@ -1,7 +1,7 @@
 package main
 
 type coordinate struct {
-	x, y int8
+	x, y int
 }
 
 type traversePoint struct {
@@ -15,79 +15,84 @@ func findShortestPath(mapInput []string) int {
 	inputMap = mapInput
 	traverseMapCache = make(map[coordinate]traversePoint)
 	steps := 0
-
 	bestSteps := &steps
 	start := findStart(mapInput)
-	var visited []coordinate
 
-	inputMap[start.y] = replaceAtIndex(inputMap[start.y], 'a'-1, start.x)
+	inputMap[start.y] = replaceAtIndex(inputMap[start.y], 'a', start.x)
 
 	prepCache()
 
-	// for i := 0; steps == 0 && i < 1000; i++ {
-	// fmt.Println("Starting for depth ", i)
-	traverseMap(visited, start, bestSteps, 1000)
-	// }
+	traverseMap(start, bestSteps, 1000)
 
 	return steps
 }
 
-func traverseMap(past []coordinate, coo coordinate, bestSteps *int, max int) {
-	// for ;*bestSteps == 0; {
+func traverseMap(coo coordinate, bestSteps *int, max int) {
+	stepCount := make(map[coordinate]int)
+	stepCount[coo]++
+	var from coordinate = coo
+	pointsToTravel := whereTo(stepCount, coo)
+	for *bestSteps == 0 {
+		newList := make(map[coordinate]bool)
+		for c := range pointsToTravel {
+			from = c
+			if inputMap[c.y][c.x] == 'E' {
+				*bestSteps = stepCount[c] + 1
+				return
+			}
 
+			tempList := whereTo(stepCount, c)
+			for v := range tempList {
+				stepCount[v] = stepCount[from] + 1
+				newList[v] = true
+			}
+		}
 
-	// }
-	past = append(past, coo)
-
-	if len(past) > max {
-		return
+		pointsToTravel = newList
 	}
+}
 
-	if  && *bestSteps < len(past)-1 {
-		return
-	}
-	if inputMap[coo.y][coo.x] == 'E' {
-		*bestSteps = len(past) - 1
-		return
-	}
+func whereTo(past map[coordinate]int, coo coordinate) map[coordinate]bool {
+	pointsToTravel := make(map[coordinate]bool)
 
 	left := coordinate{coo.x - 1, coo.y}
-	if traverseMapCache[coo].left && !contains(past, left) {
-		traverseMap(past, left, bestSteps, max)
+	if traverseMapCache[coo].left && past[left] < 1 {
+		pointsToTravel[left] = true
 	}
 
 	right := coordinate{coo.x + 1, coo.y}
-	if traverseMapCache[coo].right && !contains(past, right) {
-		traverseMap(past, right, bestSteps, max)
+	if traverseMapCache[coo].right && past[right] < 1 {
+		pointsToTravel[right] = true
 	}
 
 	up := coordinate{coo.x, coo.y - 1}
-	if traverseMapCache[coo].up && !contains(past, up) {
-		traverseMap(past, up, bestSteps, max)
+	if traverseMapCache[coo].up && past[up] < 1 {
+		pointsToTravel[up] = true
 	}
 
 	down := coordinate{coo.x, coo.y + 1}
-	if traverseMapCache[coo].down && !contains(past, down) {
-		traverseMap(past, down, bestSteps, max)
+	if traverseMapCache[coo].down && past[down] < 1 {
+		pointsToTravel[down] = true
 	}
+
+	return pointsToTravel
 }
 
 func prepCache() {
 	for y, row := range inputMap {
 		for x := 0; x < len(row); x++ {
-
-			right := coordinate{int8(x) + 1, int8(y)}
-			left := coordinate{int8(x) - 1, int8(y)}
-			up := coordinate{int8(x), int8(y) - 1}
-			down := coordinate{int8(x), int8(y) + 1}
-			coo := coordinate{int8(x), int8(y)}
+			right := coordinate{x + 1, y}
+			left := coordinate{x - 1, y}
+			up := coordinate{x, y - 1}
+			down := coordinate{x, y + 1}
+			coo := coordinate{x, y}
 
 			traverseMapCache[coo] = traversePoint{isInDistance(coo, left), isInDistance(coo, right), isInDistance(coo, up), isInDistance(coo, down)}
 		}
 	}
 }
 
-func replaceAtIndex(in string, r rune, i int8) string {
+func replaceAtIndex(in string, r rune, i int) string {
 	out := []rune(in)
 	out[i] = r
 	return string(out)
@@ -97,7 +102,7 @@ func findStart(mapInput []string) coordinate {
 	for y, row := range mapInput {
 		for x := 0; x < len(row); x++ {
 			if row[x] == 'S' {
-				return coordinate{int8(x), int8(y)}
+				return coordinate{x, y}
 			}
 		}
 	}
@@ -105,30 +110,14 @@ func findStart(mapInput []string) coordinate {
 }
 
 func isInDistance(from, to coordinate) bool {
-	if to.x < 0 || to.y < 0 || to.y >= int8(len(inputMap)) || to.x >= int8(len(inputMap[to.y])) {
+	if to.x < 0 || to.y < 0 || to.y >= len(inputMap) || to.x >= len(inputMap[to.y]) {
 		return false
 	}
-	diff := inputMap[to.y][to.x] - inputMap[from.y][from.x]
-	if diff <= 1 || diff == 255 || (inputMap[from.y][from.x] == 'z' && inputMap[to.y][to.x] == 'E') {
+	diff := float64(inputMap[to.y][to.x]) - float64(inputMap[from.y][from.x])
+
+	if diff <= 1 || ((inputMap[from.y][from.x] == 'z' || inputMap[from.y][from.x] == 'y') && inputMap[to.y][to.x] == 'E') {
 		return true
 	} else {
 		return false
 	}
-}
-
-func copyMap(from map[coordinate]bool) map[coordinate]bool {
-	newMap := make(map[coordinate]bool)
-	for k, v := range from {
-		newMap[k] = v
-	}
-	return newMap
-}
-
-func contains(s []coordinate, e coordinate) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
 }
