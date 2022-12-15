3b7b0bb64c744ad6aa89ba71149e287879a3eac7
Steffen Dangmann <steffen.dangmann@regnology.net>
2022-12-15 23:27:14 +0100

    refactored with part2

--- day15.go
+++ day15.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
-
-var yToLook int = 2000000
 
 type SBPair struct {
 	sensor, beacon Point
@@ -17,73 +15,32 @@ type Range struct {
 	isDistress bool
 }
 
+type ByRange []Range
+
+func (l ByRange) Len() int           { return len(l) }
+func (l ByRange) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByRange) Less(i, j int) bool { return l[i].from < l[j].from }
+
 func calcDistress(lines []string) int {
-	ranges := extractRanges(lines)
-
-	// count := countRanges(ranges)
-	// fmt.Println(cMap)
-	count := 0
-	for _, v := range ranges {
-		if v == 1 {
-			count++
-		}
-	}
-	return count
-}
-
-func countRanges(ranges []Range) int {
-	sum := 0
-
-	return sum
-}
-
-func extractRanges(lines []string) map[int]int {
-	completeM := make(map[Point]byte)
-
-	// var ranges []Range
+	yToLook := 2000000
 	dMap := make(map[int]int)
 	var pairs []SBPair
+
 	for _, l := range lines {
-		parts := strings.Split(l, " ")
-
-		sxPart := strings.Split(parts[2], "=")
-		sx, _ := strconv.Atoi(sxPart[1][:len((sxPart[1]))-1])
-		syPart := strings.Split(parts[3], "=")
-		sy, _ := strconv.Atoi(syPart[1][:len((syPart[1]))-1])
-		s := Point{sx, sy}
-
-		completeM[s] = 'S'
+		s, b := extractSB(l)
 		if s.y == yToLook {
 			dMap[s.x] = '2'
-			// ranges = append(ranges, Range{s.x, s.x, false})
 		}
-
-		bxPart := strings.Split(parts[8], "=")
-		bx, _ := strconv.Atoi(bxPart[1][:len((bxPart[1]))-1])
-		byPart := strings.Split(parts[9], "=")
-		by, _ := strconv.Atoi(byPart[1])
-		b := Point{bx, by}
-
-		completeM[b] = 'B'
 		if b.y == yToLook {
 			dMap[b.x] = '2'
-			// ranges = append(ranges, Range{b.x, b.x, false})
 		}
 		pairs = append(pairs, SBPair{s, b})
 	}
 
-	// pairs = nil
-	// pairs = append(pairs, SBPair{Point{8, 7}, Point{2, 10}})
 	for _, p := range pairs {
-		distance := absInt(p.sensor.x-p.beacon.x) + absInt(p.sensor.y-p.beacon.y)
+		distance := calcManhattenD(p)
 
-		startP := p.sensor
-
-		// fmt.Print("from ", startP.y-distance)
-		// fmt.Print("to ", startP.y+distance)
-		// fmt.Print("and its in range: ", startP.y+distance >= 2000000 && startP.y-distance <= 2000000)
-		if startP.y+distance >= yToLook && startP.y-distance <= yToLook {
-			fmt.Println("impact in range!")
+		if p.sensor.y+distance >= yToLook && p.sensor.y-distance <= yToLook {
 			span := distance - absInt(p.sensor.y-yToLook)
 			xFrom := p.sensor.x - span
 			xTo := p.sensor.x + span
@@ -93,46 +50,79 @@ func extractRanges(lines []string) map[int]int {
 					dMap[i] = 1
 				}
 			}
-
 		}
-		// fmt.Println()
-		// if that distance can go to y = 2000000, calc the new range in there
-		// check overlap with existing ranges
-
-		// for d := 0; d <= distance; d++ {
-		// 	for x := 0; x <= distance-d; x++ {
-		// 		fillDistress(startP.x-x, startP.y+d, &completeM, &distressM)
-		// 		fillDistress(startP.x+x, startP.y+d, &completeM, &distressM)
-		// 		fillDistress(startP.x-x, startP.y-d, &completeM, &distressM)
-		// 		fillDistress(startP.x+x, startP.y-d, &completeM, &distressM)
-		// 	}
-		// }
 	}
 
-	// for y := -10; y < 24; y++ {
-	// 	fmt.Print(y)
-	// 	for x := -8; x < 30; x++ {
-	// 		switch completeM[Point{x, y}] {
-	// 		case 0:
-	// 			fmt.Print(".")
-	// 		default:
-	// 			fmt.Print(string(completeM[Point{x, y}]))
-	// 		}
-	// 	}
-	// 	fmt.Println()
-	// }
-
-	return dMap
+	return countDistress(dMap)
 }
 
-func fillDistress(x, y int, completeM *map[Point]byte, distressM *map[int]int) {
-	if y == 2000000 {
-		toPut := Point{x, y}
-		if (*completeM)[toPut] == 0 {
-			(*completeM)[toPut] = '#'
-			(*distressM)[toPut.y]++
+func calcManhattenD(p SBPair) int {
+	distance := absInt(p.sensor.x-p.beacon.x) + absInt(p.sensor.y-p.beacon.y)
+	return distance
+}
+
+func countDistress(dMap map[int]int) int {
+	count := 0
+	for _, v := range dMap {
+		if v == 1 {
+			count++
 		}
 	}
+	return count
+}
+
+func findMissingBeacon(lines []string) int {
+	var pairs []SBPair
+	for _, l := range lines {
+		s, b := extractSB(l)
+		pairs = append(pairs, SBPair{s, b})
+	}
+
+	sum := 0
+	for y := 0; y <= 4000000; y++ {
+		go findX(pairs, y, &sum)
+	}
+	return sum
+}
+
+func findX(pairs []SBPair, y int, sum *int) {
+	var distress []Range
+	for _, p := range pairs {
+		distance := calcManhattenD(p)
+
+		if p.sensor.y+distance >= y && p.sensor.y-distance <= y {
+			span := distance - absInt(p.sensor.y-y)
+			xFrom := p.sensor.x - span
+			xTo := p.sensor.x + span
+			distress = append(distress, Range{xFrom, xTo, true})
+		}
+	}
+	sort.Sort(ByRange(distress))
+	for i, upper := 0, distress[0].to; i < len(distress)-1; i++ {
+		if upper < distress[i].to {
+			upper = distress[i].to
+		}
+		if upper < distress[i+1].from {
+			*sum = ((upper + 1) * 4000000) + y
+		}
+	}
+}
+
+func extractSB(l string) (Point, Point) {
+	parts := strings.Split(l, " ")
+
+	sxPart := strings.Split(parts[2], "=")
+	sx, _ := strconv.Atoi(sxPart[1][:len((sxPart[1]))-1])
+	syPart := strings.Split(parts[3], "=")
+	sy, _ := strconv.Atoi(syPart[1][:len((syPart[1]))-1])
+	s := Point{sx, sy}
+
+	bxPart := strings.Split(parts[8], "=")
+	bx, _ := strconv.Atoi(bxPart[1][:len((bxPart[1]))-1])
+	byPart := strings.Split(parts[9], "=")
+	by, _ := strconv.Atoi(byPart[1])
+	b := Point{bx, by}
+	return s, b
 }
 
 func absInt(x int) int {
--- main.go
+++ main.go
@@ -2,95 +2,103 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
 
-	// http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []int = readFileAsNumbers("input/day01.txt")
-	// 	fmt.Fprintln(w, "highest: ", calcCalories(lines))
-	// 	fmt.Fprintln(w, "top3: ", calcCaloriesTop3(lines))
-	// })
+	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) {
+		var lines []int = readFileAsNumbers("input/day01.txt")
+		fmt.Fprintln(w, "highest: ", calcCalories(lines))
+		fmt.Fprintln(w, "top3: ", calcCaloriesTop3(lines))
+	})
 
-	// http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day02.txt")
-	// 	fmt.Fprintln(w, "score: ", calcRPC(lines, false))
-	// 	fmt.Fprintln(w, "score2: ", calcRPC(lines, true))
-	// })
+	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day02.txt")
+		fmt.Fprintln(w, "score: ", calcRPC(lines, false))
+		fmt.Fprintln(w, "score2: ", calcRPC(lines, true))
+	})
 
-	// http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day03.txt")
-	// 	fmt.Fprintln(w, "score: ", calcRucksack(lines))
-	// 	fmt.Fprintln(w, "score of 3: ", calcRucksackOf3(lines))
-	// })
+	http.HandleFunc("/3", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day03.txt")
+		fmt.Fprintln(w, "score: ", calcRucksack(lines))
+		fmt.Fprintln(w, "score of 3: ", calcRucksackOf3(lines))
+	})
 
-	// http.HandleFunc("/4", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day04.txt")
-	// 	fmt.Fprintln(w, "score: ", calcCoverage(lines))
-	// 	fmt.Fprintln(w, "score singles: ", calcCoverageSingle(lines))
-	// })
+	http.HandleFunc("/4", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day04.txt")
+		fmt.Fprintln(w, "score: ", calcCoverage(lines))
+		fmt.Fprintln(w, "score singles: ", calcCoverageSingle(lines))
+	})
 
-	// http.HandleFunc("/5", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day05.txt")
-	// 	fmt.Fprintln(w, "crates: ", moveCrates(lines, false))
-	// 	fmt.Fprintln(w, "crates after crane 9001: ", moveCrates(lines, true))
-	// })
+	http.HandleFunc("/5", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day05.txt")
+		fmt.Fprintln(w, "crates: ", moveCrates(lines, false))
+		fmt.Fprintln(w, "crates after crane 9001: ", moveCrates(lines, true))
+	})
 
-	// http.HandleFunc("/6", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day06.txt")
-	// 	fmt.Fprintln(w, "4 char package: ", findStartOfPacket(lines[0], 4))
-	// 	fmt.Fprintln(w, "14 char package: ", findStartOfPacket(lines[0], 14))
-	// })
+	http.HandleFunc("/6", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day06.txt")
+		fmt.Fprintln(w, "4 char package: ", findStartOfPacket(lines[0], 4))
+		fmt.Fprintln(w, "14 char package: ", findStartOfPacket(lines[0], 14))
+	})
 
-	// http.HandleFunc("/7", func(w http.ResponseWriter, r *http.Request) {
-	// 	var lines []string = readFileAsStringArray("input/day07.txt")
-	// 	fmt.Fprintln(w, "folders sum below 1kk: ", calcDirectorySize(lines))
-	// 	fmt.Fprintln(w, "deleteFolder size: ", findDeleteFolder(lines))
-	// })
+	http.HandleFunc("/7", func(w http.ResponseWriter, r *http.Request) {
+		var lines []string = readFileAsStringArray("input/day07.txt")
+		fmt.Fprintln(w, "folders sum below 1kk: ", calcDirectorySize(lines))
+		fmt.Fprintln(w, "deleteFolder size: ", findDeleteFolder(lines))
+	})
 
-	// http.HandleFunc("/8", func(w http.ResponseWriter, r *http.Request) {
-	// 	treemap := readFileAsIntMap("input/day08.txt")
-	// 	trees, scenicScore := findTrees(treemap)
-	// 	fmt.Fprintln(w, "visible trees: ", trees)
-	// 	fmt.Fprintln(w, "highest scenic score: ", scenicScore)
-	// })
+	http.HandleFunc("/8", func(w http.ResponseWriter, r *http.Request) {
+		treemap := readFileAsIntMap("input/day08.txt")
+		trees, scenicScore := findTrees(treemap)
+		fmt.Fprintln(w, "visible trees: ", trees)
+		fmt.Fprintln(w, "highest scenic score: ", scenicScore)
+	})
 
-	// http.HandleFunc("/9", func(w http.ResponseWriter, r *http.Request) {
-	// 	lines := readFileAsStringArray("input/day09.txt")
-	// 	fmt.Fprintln(w, "coverage small snek: ", calcMapCoverage(lines, 1))
-	// 	fmt.Fprintln(w, "coverage big snek: ", calcMapCoverage(lines, 9))
-	// })
+	http.HandleFunc("/9", func(w http.ResponseWriter, r *http.Request) {
+		lines := readFileAsStringArray("input/day09.txt")
+		fmt.Fprintln(w, "coverage small snek: ", calcMapCoverage(lines, 1))
+		fmt.Fprintln(w, "coverage big snek: ", calcMapCoverage(lines, 9))
+	})
 
-	// http.HandleFunc("/10", func(w http.ResponseWriter, r *http.Request) {
-	// 	lines := readFileAsStringArray("input/day10.txt")
-	// 	cycles := [6]int{20, 60, 100, 140, 180, 220}
-	// 	fmt.Fprintln(w, "x sum: ", runCycles(lines, cycles[:], w))
-	// })
+	http.HandleFunc("/10", func(w http.ResponseWriter, r *http.Request) {
+		lines := readFileAsStringArray("input/day10.txt")
+		cycles := [6]int{20, 60, 100, 140, 180, 220}
+		fmt.Fprintln(w, "x sum: ", runCycles(lines, cycles[:], w))
+	})
 
-	// http.HandleFunc("/11", func(w http.ResponseWriter, r *http.Request) {
-	// 	lines := readFileAsStringArray("input/day11.txt")
-	// 	fmt.Fprintln(w, "monkees after 20: ", chaseMonkyes(lines, 20))
-	// 	fmt.Fprintln(w, "monkees after 10000: ", chaseMonkyes(lines, 10000))
-	// })
+	http.HandleFunc("/11", func(w http.ResponseWriter, r *http.Request) {
+		lines := readFileAsStringArray("input/day11.txt")
+		fmt.Fprintln(w, "monkees after 20: ", chaseMonkyes(lines, 20))
+		fmt.Fprintln(w, "monkees after 10000: ", chaseMonkyes(lines, 10000))
+	})
 
-	// http.HandleFunc("/12", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintln(w, "shortestPath start from S", findShortestPath(readFileAsStringArray("input/day12.txt"), 'S'))
-	// 	fmt.Fprintln(w, "shortestPath start from a", findShortestPath(readFileAsStringArray("input/day12.txt"), 'a'))
-	// })
+	http.HandleFunc("/12", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "shortestPath start from S", findShortestPath(readFileAsStringArray("input/day12.txt"), 'S'))
+		fmt.Fprintln(w, "shortestPath start from a", findShortestPath(readFileAsStringArray("input/day12.txt"), 'a'))
+	})
 
-	// http.HandleFunc("/13", func(w http.ResponseWriter, r *http.Request) {
-	// 	sum, decoderKey := calcSumOfIndicese(readFileAsStringArray("input/day13.txt"))
-	// 	fmt.Fprintln(w, "sum of indices ", sum)
-	// 	fmt.Fprintln(w, "decoder key", decoderKey)
-	// })
+	http.HandleFunc("/13", func(w http.ResponseWriter, r *http.Request) {
+		sum, decoderKey := calcSumOfIndicese(readFileAsStringArray("input/day13.txt"))
+		fmt.Fprintln(w, "sum of indices ", sum)
+		fmt.Fprintln(w, "decoder key", decoderKey)
+	})
 
-	// http.HandleFunc("/14", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintln(w, "landed sand ", calcSandAtRest(readFileAsStringArray("input/day14.txt"), false))
-	// 	fmt.Fprintln(w, "landed sand with floor", calcSandAtRest(readFileAsStringArray("input/day14.txt"), true))
-	// })
+	http.HandleFunc("/14", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "landed sand ", calcSandAtRest(readFileAsStringArray("input/day14.txt"), false))
+		fmt.Fprintln(w, "landed sand with floor", calcSandAtRest(readFileAsStringArray("input/day14.txt"), true))
+	})
 
-	// log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/15", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "distress amount: ", calcDistress(readFileAsStringArray("input/day15.txt")))
+		start := time.Now()
+		fmt.Fprintln(w, "Lost Beacon tuning: ", findMissingBeacon(readFileAsStringArray("input/day15.txt")))
+		elapsed := time.Since(start)
+		fmt.Fprintln(w, "took ", elapsed)
+	})
 
-	fmt.Println("distress ", calcDistress(readFileAsStringArray("input/day15.txt")))
-
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
