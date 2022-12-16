d7d5ecb39f631782f31de84b6458898489ef0794
Steffen Dangmann <steffen.dangmann@regnology.net>
2022-12-16 18:12:39 +0100

    fast but not highest

---
+++ day16.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Pipe struct {
+	name     string
+	pressure int
+	leadsTo  []string
+}
+
+type FromTo struct {
+	from string
+	to   string
+}
+
+type Action struct {
+	pressureGain int
+	distance     int
+	targetPipe   Pipe
+}
+
+type ByPressure []Pipe
+
+func (l ByPressure) Len() int           { return len(l) }
+func (l ByPressure) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByPressure) Less(i, j int) bool { return l[i].pressure > l[j].pressure }
+
+func calcPressure(lines []string) int {
+	sum := 0
+	pipes := extractPipes(lines)
+
+	distances := make(map[FromTo]int)
+	for _, f := range pipes {
+		if f.pressure == 0 && f.name != "AA" {
+			continue
+		}
+		from := f.name
+		for _, t := range pipes {
+			if t.pressure == 0 {
+				continue
+			}
+			if f.name != t.name {
+				shortestPath := 9999
+				var history []string
+				findShortestDistance(from, from, t.name, pipes, &shortestPath, 0, history)
+				distances[FromTo{from, t.name}] = shortestPath
+			}
+		}
+	}
+
+	var valuePipes []Pipe
+	for _, p := range pipes {
+		if p.pressure != 0 {
+			valuePipes = append(valuePipes, p)
+		}
+	}
+	sort.Sort(ByPressure(valuePipes))
+
+	currentPipe := "AA"
+	for i := 0; i < 30; i++ {
+		highestNextPipe := findHighest(currentPipe, distances, valuePipes, 30-i)
+		if highestNextPipe.pressureGain != 0 {
+			sum += highestNextPipe.pressureGain
+			i += highestNextPipe.distance + 1
+			currentPipe = highestNextPipe.targetPipe.name
+			remove(valuePipes, highestNextPipe.targetPipe)
+		} else {
+			break
+		}
+	}
+
+	return sum
+}
+
+func findHighest(currentPipe string, distances map[FromTo]int, valuePipes []Pipe, timeLeft int) Action {
+	var bestMove Action
+	for _, p := range valuePipes {
+		distance := distances[FromTo{currentPipe, p.name}]
+		if (distance + 1) > timeLeft {
+			continue
+		}
+		total := (timeLeft - distance - 1) * p.pressure
+		if total > bestMove.pressureGain {
+			bestMove.pressureGain = total
+			bestMove.targetPipe = p
+			bestMove.distance = distance
+		}
+	}
+	return bestMove
+}
+
+func remove(s []Pipe, r Pipe) []Pipe {
+	for i, v := range s {
+		if v.name == r.name {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
+}
+
+func findShortestDistance(start, from, to string, pipes map[string]Pipe, shortest *int, steps int, history []string) {
+	if contains(history, from) {
+		return
+	}
+	if contains(pipes[from].leadsTo, to) {
+		if steps < *shortest {
+			*shortest = steps + 1
+		}
+		return
+	}
+	for _, v := range pipes[from].leadsTo {
+		history = append(history, from)
+		findShortestDistance(start, v, to, pipes, shortest, steps+1, history[:])
+	}
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func extractPipes(lines []string) map[string]Pipe {
+	pipes := make(map[string]Pipe)
+	for _, v := range lines {
+		parts := strings.Split(v, " ")
+		name := parts[1]
+		pressurePart := strings.Split(parts[4], "=")
+		pressure, _ := strconv.Atoi(pressurePart[1][:len(pressurePart[1])-1])
+
+		var leadsTo []string
+		for i := 9; i < len(parts); i++ {
+			if i == len(parts)-1 {
+				leadsTo = append(leadsTo, parts[i])
+			} else {
+				leadsTo = append(leadsTo, parts[i][:2])
+			}
+		}
+		pipes[name] = Pipe{name, pressure, leadsTo}
+	}
+	return pipes
+}
